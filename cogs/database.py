c0d12c33607938882c3f4a9d1d3b9ccb520ff73e
Anshul <anshulbirla@gmail.com>
2020-09-07 00:03:11 -0700

    add/remove cat methods implemented

--- cogs/database.py
+++ cogs/database.py
@@ -14,20 +14,28 @@ class DatabaseCog(commands.Cog, name="Database"):
             password = config.sql_password,
             database = config.sql_database
         )
-        self.mycursor = self.mydb.cursor()
+        try: 
+            self.mycursor = self.mydb.cursor()
+        except Exception as e:
+             print("Exeception occured:{}".format(e))
+
         self.mycursor.execute('USE new_schema')
 
     #create a new table 
     @commands.command() 
-    async def newtable(self, ctx, *args):
-        command = 'CREATE TABLE '
-        command += args[0] + ' ('
-        for arg in args[1:]:
-            command += arg + ' VARCHAR(255), '
-        command = command[:-2]
-        command += ')'
-        self.mycursor.execute(command)
-        ctx.send("FUNCTION NOT IMPLEMENTED")
+    async def addCollection(self, ctx, *args):
+        try: 
+            command = 'CREATE TABLE '
+            command += args[0] + ' ('
+            for arg in args[1:]:
+                command += arg + ' VARCHAR(255), '
+            command = command[:-2]
+            command += ')'
+            self.mycursor.execute(command)
+            await ctx.send(f"`{args[0]}` collection created succesfully with `{len(args)-1}` categorie(s)`")
+        except Exception as e:
+             print("Exeception occured:{}".format(e))
+             await ctx.send("Unable to create table. Sorry :(")
 
     ##cadd a point to table 
     @commands.command()
@@ -76,6 +84,35 @@ class DatabaseCog(commands.Cog, name="Database"):
         ctx.send("FUNCTION NOT IMPLEMENTED")
     
     #alter table
+    @commands.command()
+    async def addCategory(self, ctx, *args):
+        try:
+            command = 'ALTER TABLE '
+            command += args[0]
+            for arg in args[1:]:
+                command += ' ADD ' + arg + ' VARCHAR(255), '
+            command = command[:-2]
+            print(command)
+            self.mycursor.execute(command)
+            await ctx.send(f"Succesfully added `{len(args)-1}` categories` to `{args[0]}` collection")
+        except Exception as e:
+                print("Exeception occured:{}".format(e))
+                await ctx.send("Unable to create table. Sorry :(")
+
+    @commands.command()
+    async def removeCategory(self, ctx, *args):
+        try:
+            command = 'ALTER TABLE '
+            command += args[0]
+            for arg in args[1:]:
+                command += ' DROP ' + arg + ', '
+            command = command[:-2]
+            self.mycursor.execute(command)
+            await ctx.send(f"Succesfully dropped `{len(args)-1}` categories` from `{args[0]}` collection")
+        except Exception as e:
+                print("Exeception occured:{}".format(e))
+                await ctx.send("Unable to create table. Sorry :(")
+
 
 
 #default table functionality 
